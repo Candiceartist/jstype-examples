f5658865f51af6977b487f3c10e38718b612f6b9
Candice Jovon Juarez <candice.artist@gmail.com>
2023-01-23 00:03:17 -0500

    Add files via upload

---
+++ practice-types.js
@@ -0,0 +1,60 @@
+//String
+
+const  name = 'Candice';
+const start = 'Nice to meet you';
+const quote = 'Live long and prosper';
+const greeting = 'You look marvelous';
+const who = 'I am the artist';
+const how = 'Through years of experience';
+const when = 'when I shared my gifts with the world';
+const what = 'The world gave me my name';
+const where = 'On the stage';
+const why = 'Here I am';
+
+console.log ('start');
+
+
+//integer
+const age = 36;
+const siblings = 2;
+const parents = 2;
+const pet = 1;
+const eyes = 2;
+const hands = 2;
+const ears = 2;
+const heart = 1;
+const fingers = 10;
+const toes = 10;
+
+
+//Boolean
+const isanartist = true;
+
+//Array
+const colors = ['blue', 'green', 'yellow'];
+const cars = ['ford', 'honda', 'kia' 'hyundai'];
+const trees = ['spruce', 'maple', 'pine'];
+const location = ['here', 'there', 'everywhere'];
+const weather = ['sun', 'rain', 'snow'];
+const flowers = ['rose', 'violet', 'daisy'];
+const teas = ['oolong', 'chamomile', 'hibiscus'];
+const pets = ['bird', 'cat', 'dog'];
+const flavors = ['chocolate', 'strawberry', 'vanilla'];
+const flatware = ['spoon', 'fork', 'knife'];
+
+//object
+const person1 = new person( Candice, Jersey City, Software Engineer)
+const person2 = new person( Jose, Brooklyn, Train Conductort)  
+const person3 = new person( Joann, Manhattan, Housewife)
+const person4 = new person( David, Manhattan, Accountant)
+const person5 = new person( Alison, East Rutherford, Nurse)
+const person6 = new person( Matteo, Rosetto-Italy, Manager)
+const person7 = new person( Gabriel, North Bergen, Teachert)
+const person8 = new person( Danny, Union City, Whole Digger)
+const person9 = new person( Bentley, Upstate, Dog)
+const person10 = new person( Mickey, Disney, Mouse)
+
+
+//concatentation with template string
+const hello : 'My name is ${name} and I am ${age};
+
